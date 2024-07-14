9316e3de117693b2f3f04028b0e2552ed0cacb13
rayyh <rayyounghong@gmail.com>
2024-07-14 12:52:33 +0800

    feat: update mysql services

--- services/mysql/init/20-create-user.sql
+++ services/mysql/init/20-create-user.sql
@@ -1,6 +1,7 @@
--- ALTER USER 'root'@'%' IDENTIFIED BY 'secret123';
--- CREATE USER 'dev'@'%' IDENTIFIED BY 'secret123';
--- GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'secret';
+CREATE USER 'dev'@'%' IDENTIFIED WITH mysql_native_password BY 'secret';
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'dev'@'%' WITH GRANT OPTION;
 
 CREATE DATABASE IF NOT EXISTS dev;
